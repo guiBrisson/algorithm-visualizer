3a8606bb96c6f5ceb86e5a489920f24b9d2efb90
guiBrisson <guibrisson@hotmail.com>
2023-09-11 14:12:03 -0300

    Theming

--- app/src/main/java/me/brisson/algorithm_visualizer/ui/theme/Color.kt
+++ app/src/main/java/me/brisson/algorithm_visualizer/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package me.brisson.algorithm_visualizer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val background = Color(0xff242424)
+val background_dark = Color(0xff1C1C1C)
+val surface = Color(0xff2F2F2F)
+val stroke = Color(0xff373737)
+val primary = Color(0xff1BD96A)
--- app/src/main/java/me/brisson/algorithm_visualizer/ui/theme/Type.kt
+++ app/src/main/java/me/brisson/algorithm_visualizer/ui/theme/Type.kt
@@ -2,33 +2,84 @@ package me.brisson.algorithm_visualizer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import me.brisson.algorithm_visualizer.R
 
-// Set of Material typography styles to start with
+val interFontFamily = FontFamily(
+    Font(R.font.inter_thin, weight = FontWeight.Thin),
+    Font(R.font.inter_light, weight = FontWeight.Light),
+    Font(R.font.inter_regular, weight = FontWeight.Normal),
+    Font(R.font.inter_medium, weight = FontWeight.Medium),
+    Font(R.font.inter_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.inter_bold, weight = FontWeight.Bold),
+)
+
+val jetbrainsMonoFontFamily = FontFamily(
+    Font(R.font.jetbrains_mono_thin, weight = FontWeight.Thin),
+    Font(R.font.jetbrains_mono_light, weight = FontWeight.Light),
+    Font(R.font.jetbrains_mono_regular, weight = FontWeight.Normal),
+    Font(R.font.jetbrains_mono_medium, weight = FontWeight.Medium),
+    Font(R.font.jetbrains_mono_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.jetbrains_mono_bold, weight = FontWeight.Bold),
+)
+
+val display2Style = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 25.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val display1Style = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 22.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val headlineStyle = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val titleStyle = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val subheadStyle = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val body1Style = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 14.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val captionStyle = TextStyle(
+    fontFamily = interFontFamily,
+    fontSize = 12.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val consoleStyle = TextStyle(
+    fontFamily = jetbrainsMonoFontFamily,
+    fontSize = 12.sp,
+    fontWeight = FontWeight.Normal
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    labelSmall = captionStyle,
+    bodyMedium = body1Style,
+    titleSmall = subheadStyle,
+    titleMedium = titleStyle,
+    headlineMedium = headlineStyle,
+    displaySmall = display1Style,
+    displayMedium = display2Style,
 )
